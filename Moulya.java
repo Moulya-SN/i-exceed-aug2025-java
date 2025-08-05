e05876652c7b631e610f9d26240037bd349045ed
Moulya-SN <moulyasn17@gmail.com>
2025-08-05 22:53:43 +0530

    Add files via upload
    To explore dataypes

---
+++ Moulya.java
@@ -0,0 +1,26 @@
+public class Moulya {
+    public static void main(String[] args) {
+        // integer type
+        int ix = 12;
+      
+        // Floating-point types
+        float fx = 3.14f;
+        double dx = 3.14159265358979;
+
+        // Character type
+        char cx = 'A';
+
+        // Boolean type
+        boolean boolTrue = true;
+        boolean boolFalse = false;
+
+        // Print all data types
+        
+        System.out.println("Int: " + ix);
+        System.out.println("Float: " + fx);
+        System.out.println("Double: " + dx);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean (true): " + boolTrue);
+        System.out.println("Boolean (false): " + boolFalse);
+    }
+}
