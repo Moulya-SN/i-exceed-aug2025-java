3e2c12514e6d3d0004e5da6247378a36d587c843
Moulya-SN <moulyasn17@gmail.com>
2025-08-06 15:46:08 +0530

    Simple code on Method Overloading

--- methodOver.java
+++ methodOver.java
@@ -1,19 +1,18 @@
-public class Calculator {
-    public int add(int a, int b) {
+public class methodOver {
+    int add(int a, int b) {
         return a + b;
     }
-    public int add(int a, int b, int c) {
+    int add(int a, int b, int c) {
         return a + b + c;
     }
-    public double add(double a, double b) {
+    double add(double a, double b) {
         return a + b;
     }
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-
-        System.out.println("Sum of 2 and 3: " + calc.add(2, 3));             
-        System.out.println("Sum of 2, 3 and 4: " + calc.add(2, 3, 4));       
-        System.out.println("Sum of 2.5 and 3.5: " + calc.add(2.5, 3.5));    
+        MethodOver calc = new MethodOver();
+        System.out.println(calc.add(5, 10));          
+        System.out.println(calc.add(1, 2, 3));        
+        System.out.println(calc.add(2.5, 3.7));       
     }
 }
