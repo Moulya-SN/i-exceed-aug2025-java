02c90235fb762ccb7df38456cd0276606ed69eaf
Moulya-SN <moulyasn17@gmail.com>
2025-09-12 14:56:48 +0530

    Add files via upload

---
+++ Currency conversion/proof.js
@@ -0,0 +1,8 @@
+const Big = require("big.js");
+console.log(new Big(0.1).plus(0.2).toString());
+console.log(new Big(0.01).times(100).toString());
+ 
+console.log(0.1 + 0.2);
+ 
+console.log(new Big(0.1).plus(0.2).toString());
+ 
